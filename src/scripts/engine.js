89dd5d6867a1a580f71818ef52d09693d5d2fcb0
Hugo Cezar <hugocssouza@gmail.com>
2023-12-06 21:13:40 -0400

    Incrementado a funcionalidade da vida e trocado o da pagina completa

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -4,6 +4,7 @@ const state = {
         enemy: document.querySelector(".enemy"),
         timeLeft: document.querySelector("#time"),
         score: document.querySelector("#score"),
+        life: document.querySelector("#life"),
     },
     values: {
         timerId: null,
@@ -11,10 +12,10 @@ const state = {
         hitPosition: 0,
         result: 0,
         currentTime: 60,
-        
+        restantLife: 3,
     },
     actions: {
-        countDownTimerId: setInterval(countDown, 1000),
+        countDownTimerId: setInterval(countTime, 1000),
     },
 };
 
@@ -41,18 +42,18 @@ function addListenerHitBox(){
                 state.view.score.textContent = state.values.result;
                 state.values.hitPosition = null;
                 playsound();
+            }else{
+                countLife();
             }
         })
     })
 }
 
-function countDown(){
+function countTime(){
     state.values.currentTime--;
     state.view.timeLeft.textContent = state.values.currentTime;
-    if(state.values.currentTime <= 0){
-        clearInterval(state.actions.countDownTimerId)
-        clearInterval(state.values.timerId)
-        alert(`Caboooo! Você fez ${state.values.result} pontos!`);
+    if(state.values.currentTime <= 0){  
+        gameover();
     }
 }
 
@@ -60,6 +61,20 @@ function playsound(){
     let audio = new Audio("./src/audios/hit.m4a");
     audio.volume = 0.4;
     audio.play();
+}
+
+function countLife(){
+    state.values.restantLife--;
+    state.view.life.textContent = state.values.restantLife;
+    if(state.values.restantLife <= 0){
+        gameover();
+    }
+}
+
+function gameover(){
+    clearInterval(state.actions.countDownTimerId)
+    clearInterval(state.values.timerId)
+    alert(`Caboooo! Você fez ${state.values.result} pontos!`);
 }
 
 function main(){
