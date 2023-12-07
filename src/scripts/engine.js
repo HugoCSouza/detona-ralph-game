21998634438574aee71f72487bfdd219a077514d
Hugo Cezar <hugocssouza@gmail.com>
2023-12-06 20:45:50 -0400

    Jogo finalizado de acordo com o desafio

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,1 +1,70 @@
-alert('Hello word');
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"), //busca no documento seletores css 
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time"),
+        score: document.querySelector("#score"),
+    },
+    values: {
+        timerId: null,
+        gameVelocity: 1000, //milisegundos
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+        
+    },
+    actions: {
+        countDownTimerId: setInterval(countDown, 1000),
+    },
+};
+
+function randomSquare(){
+    state.view.squares.forEach((square) => { //itera sobre o array pra cada valor
+        square.classList.remove("enemy");  //exclui o seletor enemy
+    })
+
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber]; //seleciona o quadrado aleatório
+    randomSquare.classList.add("enemy");
+    state.values.hitPosition = randomSquare.id // pegando o id do elemento css
+}
+
+function moveEnemy(){
+    state.values.timerId = setInterval(randomSquare, state.values.gameVelocity); //utiliza o valor da velocidade para mover o bonequinho
+}
+
+function addListenerHitBox(){
+    state.view.squares.forEach((square) => {            
+        square.addEventListener("mousedown", () => {    //verifica se o quadrado foi clicado do mouse ("mousedown")
+            if(square.id === state.values.hitPosition){ //verifca se o id do quadrado que estou clicando é igual no hitposition
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playsound();
+            }
+        })
+    })
+}
+
+function countDown(){
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+    if(state.values.currentTime <= 0){
+        clearInterval(state.actions.countDownTimerId)
+        clearInterval(state.values.timerId)
+        alert(`Caboooo! Você fez ${state.values.result} pontos!`);
+    }
+}
+
+function playsound(){
+    let audio = new Audio("./src/audios/hit.m4a");
+    audio.volume = 0.4;
+    audio.play();
+}
+
+function main(){
+    moveEnemy();
+    addListenerHitBox();
+};
+
+main();
